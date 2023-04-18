587c5c68b39dfba2d67eebad4ddadae2b1dcd4d0
Gustav Kysela <kysela.gustav@elev.ga.ntig.se>
2023-04-18 14:49:46 +0200

    bruh

---
+++ src/Book.java
@@ -0,0 +1,41 @@
+public class Book {
+    private String title;
+    private String content;
+    private boolean isBorrowed;
+    private User borrower;
+
+    public Book(String title, String content){
+        this.title = title;
+        this.content = content;
+        isBorrowed = false;
+        borrower = null;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook(User user){
+        if (!isBorrowed){
+            isBorrowed = true;
+            borrower = user;
+        } else {
+            System.out.println("Booken är redan utlånad av "+ borrower.getName());
+        }
+    }
+
+    public void returnbook(){
+        isBorrowed = false;
+        borrower = null;
+    }
+
+    public void readbook(){
+        System.out.println("Titel "+ title);
+        System.out.println(content);
+    }
+
+}
---
+++ src/BorrowBook.java
@@ -0,0 +1,50 @@
+public class BorrowBook {
+    private Book[] books;
+    public User[] users;
+
+    public BorrowBook(Book[] books, User[] users){
+        this.books = books;
+        this.users = users;
+    }
+
+    public boolean borrowBook(int bookIndex, int userIndex) {
+        if (bookIndex < 0 || bookIndex >= books.length) {
+            System.out.println("Invalid book index");
+            return false;
+        }
+
+        Book book = books[bookIndex];
+        if (book.isBorrowed()) {
+            System.out.println("Boken är redan utlånad");
+            return false;
+        }
+
+        if (userIndex < 0 || userIndex >= users.length) {
+            System.out.println("Invalid user index");
+            return false;
+        }
+
+        User user = users[userIndex];
+        book.borrowBook(user);
+        System.out.println(book.getTitle() + " är lånad av " + user.getName());
+        return true;
+    }
+
+    public boolean returnBook(int bookIndex) {
+        if (bookIndex < 0 || bookIndex >= books.length) {
+            System.out.println("Invalid book index");
+            return false;
+        }
+
+        Book book = books[bookIndex];
+        if (!book.isBorrowed()) {
+            System.out.println("Boken är inte utlånad");
+            return false;
+        }
+
+        book.returnbook();
+        System.out.println(book.getTitle() + " är tillbaka lämnad");
+        return true;
+    }
+
+}
